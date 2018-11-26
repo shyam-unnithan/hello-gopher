67e7dde18d408afb7890ff39183c8469064dc9a5
Shyam Unnithan <shyam.unnithan@anz.com>
2018-11-26 20:00:02 +0530

    Update makefile and the Dockerfile to reflect the update path

--- make.sh
+++ make.sh
@@ -2,6 +2,6 @@
 
 # Compile golang service with dependencies
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
-cp main /go/bin/main
+
 #Create image using the scratch dockerfile
 #docker build -t gopher:latest -f Dockerfile .
